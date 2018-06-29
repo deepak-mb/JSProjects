c97ef0f0f11cd19eae4696cabf6b01f473a02f9c
Deepak <deepak.mb@panorbit.in>
2018-06-29 11:07:54 +0530

    Completed Loan Calculator Project

--- LoanCalculator/app.js
+++ LoanCalculator/app.js
@@ -1,7 +1,11 @@
-document.querySelector('#loan-form').addEventListener('submit', calculateLoan);
+document.querySelector('#loan-form').addEventListener('submit', function(e){
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'block';
+    setTimeout(calculateResults, 2000);
+    e.preventDefault();
+});
 
-function calculateLoan(e){
-    e.preventDefault(); 
+function calculateResults(){
     const amount = document.getElementById('amount');
     const interest = document.getElementById('interest');
     const years = document.getElementById('years');
@@ -9,5 +13,38 @@ function calculateLoan(e){
     const totalPayment = document.getElementById('total-payment');
     const totalInterest = document.getElementById('total-interest');
 
-    console.log(amount, interest, years, monthlyPayment, totalPayment, totalInterest);
+    const principal = parseFloat(amount.value);
+    const calculatedInterest = parseFloat(interest.value) /100 / 12;
+    const calculatedPayments = parseFloat(years.value) * 12;
+
+    //Compute monthly payments
+    const x = Math.pow(1 + calculatedInterest, calculatedPayments);
+    const monthly = (principal * x * calculatedInterest) / (x - 1);
+
+    if(isFinite(monthly)){
+        monthlyPayment.value = monthly.toFixed(2);
+        totalPayment.value = (monthly * calculatedPayments).toFixed(2);
+        totalInterest.value = ((monthly * calculatedPayments) - principal).toFixed(2);
+        document.getElementById('results').style.display = 'block';
+        document.getElementById('loading').style.display = 'none';
+
+    }else{
+        showError('Please check your numbers!');
+    }
+}
+
+function showError(error){
+    document.getElementById('results').style.display = 'none';
+    document.getElementById('loading').style.display = 'none';
+    const errorDiv = document.createElement('div');
+    errorDiv.className = 'alert alert-danger';
+    errorDiv.appendChild(document.createTextNode(error));
+    const card = document.querySelector('.card');
+    const heading = document.querySelector('.heading');
+    card.insertBefore(errorDiv, heading);
+    setTimeout(clearError, 2000);
+}
+
+function clearError(){
+    document.querySelector('.alert').remove();
 }
