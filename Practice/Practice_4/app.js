167beb4b4f07ae03b1f4ca0699592977f10c1f1b
Deepak <deepak25mb@gmail.com>
2018-07-17 07:50:23 +0530

    Learnt Promise again

--- Practice/Practice_4/app.js
+++ Practice/Practice_4/app.js
@@ -1,51 +1,55 @@
 document.getElementById('button1').addEventListener('click', getText);
-document.getElementById('button2').addEventListener('click', getJson);
+document.getElementById('button2').addEventListener('click', getPosts);
 document.getElementById('button3').addEventListener('click', getExternalApi);
 
-let output;
+
 function getText() {
-    fetch('test.txt')
-        .then(response => {
+    fetch('test1.txt')
+        .then(function (response) {
             return response.text();
-            console.log(response.text());
-        }).then(data => {
-            console.log(data);
+        })
+        .then(function (data) {
             document.getElementById('output').innerHTML = data;
-        }).catch(err => {
-            console.log(err);
+
+        }).catch(function (error) {
+            console.log(`Error: ${error}`);
         });
 }
 
-function getJson() {
+function getPosts() {
     fetch('posts.json')
-        .then(response => {
+        .then(function (response) {
             return response.json();
         })
-        .then(data => {
-            console.log(data);
+        .then(function (posts) {
+            console.log(posts);
             let output = '';
-            data.forEach(function (post) {
-                output += `<li>${post.title}: ${post.body}</li>`;
+
+            posts.forEach(function (post) {
+                console.log(post);
+                output += `<li>${post.title}: ${post.body}</li>`
             });
             document.getElementById('output').innerHTML = output;
         })
-        .catch(err => {
-            console.log(err);
+        .catch(function (error) {
+            console.log(error);
         });
 }
 
 function getExternalApi() {
     fetch('https://api.github.com/users')
-        .then(response => {
+        .then(function (response) {
+            //console.log(response);
             return response.json();
-        }).then(data => {
-            console.log(data);
+        }).then(function (users) {
+            //console.log(users);
             let output = '';
-            data.forEach(function (userName) {
-                output += `<li><a href='${userName.avatar_url}' target=_blank>${userName.login}</li>`
-            });
+            users.forEach(function (user) {
+                output += `<li><a href=${user.avatar_url} target=blank>${user.login}</a></li>`
+            })
             document.getElementById('output').innerHTML = output;
-        }).catch(err => {
-            console.log(err);
+        })
+        .catch(function (error) {
+            console.log(error);
         });
 }
