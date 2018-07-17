6c66450e66b882bdac64e350c4485fa26a1f0afa
Deepak <deepak.mb@panorbit.in>
2018-07-17 17:45:31 +0530

    Added HttpLibraryVersion 2

---
+++ HTTPLibraryVersion2/easyhttp.js
@@ -0,0 +1,26 @@
+class EasyHTTP {
+    get(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url)
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+        });
+    }
+
+    post(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                method: 'POST',
+                headers: {
+                    'Content-type': 'application/json'
+                },
+                body: JSON.stringify(data)
+            })
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+        });
+    }
+
+}
