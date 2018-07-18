009b9bfe6698b0fddbc3eafe775aaa2b84e5042c
Deepak <deepak25mb@gmail.com>
2018-07-18 06:57:27 +0530

    HTTPLibraryVersion2 completed

--- HTTPLibraryVersion2/app.js
+++ HTTPLibraryVersion2/app.js
@@ -2,4 +2,23 @@ const http = new EasyHTTP();
 
 http.get('https://jsonplaceholder.typicode.com/users')
     .then(data => console.log(data))
-    .catch(err => console.log(err));
+    .catch(error => console.log(error));
+
+const data = {
+    name: 'John Doe',
+    username: 'JohnDoe',
+    email: 'john@gmail.com'
+}
+
+// http.post('https://jsonplaceholder.typicode.com/users', data)
+//     .then(data => console.log(data))
+//     .catch(error => console.log(error));
+
+
+http.put('https://jsonplaceholder.typicode.com/users/2', data)
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
+
+http.delete('https://jsonplaceholder.typicode.com/users/10')
+    .then(data => console.log(data))
+    .catch(error => console.log(error));
--- HTTPLibraryVersion2/easyhttp.js
+++ HTTPLibraryVersion2/easyhttp.js
@@ -11,16 +11,44 @@ class EasyHTTP {
     post(url, data) {
         return new Promise((resolve, reject) => {
             fetch(url, {
-                method: 'POST',
-                headers: {
-                    'Content-type': 'application/json'
-                },
-                body: JSON.stringify(data)
-            })
+                    method: 'POST',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
                 .then(response => response.json())
                 .then(data => resolve(data))
                 .catch(error => reject(error));
         });
     }
 
+    put(url, data) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'PUT',
+                    headers: {
+                        'Content-type': 'application/json'
+                    },
+                    body: JSON.stringify(data)
+                })
+                .then(response => response.json())
+                .then(data => resolve(data))
+                .catch(error => reject(error));
+        });
+    }
+
+    delete(url) {
+        return new Promise((resolve, reject) => {
+            fetch(url, {
+                    method: 'DELETE',
+                    headers: {
+                        'Content-type': 'application/json'
+                    }
+                })
+                .then(response => response.json())
+                .then(() => resolve('Resource deleted...'))
+                .catch(error => reject(error));
+        });
+    }
 }
