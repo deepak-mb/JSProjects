84d605a9d2870c70a90d1d8aad92d6c5d9a4f148
Deepak <deepak25mb@gmail.com>
2018-07-20 07:01:14 +0530

    Completed second part of fetching the api from GitHub

---
+++ GitHubFinder/app.js
@@ -0,0 +1,22 @@
+const github = new Github;
+const searchUser = document.getElementById('searchUser');
+
+searchUser.addEventListener('keyup', (e) => {
+    const userText = e.target.value;
+
+    if (userText !== '') {
+        github.getUser(userText)
+            .then(data => {
+                console.log(data);
+                if (data.profile.message === 'Not found') {
+
+                } else {
+                    console.log(data.profile);
+                }
+
+            });
+    } else {
+
+    }
+
+})
---
+++ GitHubFinder/github.js
@@ -0,0 +1,14 @@
+class Github {
+    constructor() {
+        this.client_id = 'f825e74c979ccab05b35';
+        this.client_secret = '6e3f66a73ead0138fe724b387efad7d2a6427cd0';
+    }
+
+    async getUser(user) {
+        const profileResponse = await fetch(`https://api.github.com/users/${user}?client_id=${this.client_id}&client_secret=${this.client_secret}`);
+        const profileData = await profileResponse.json();
+        return {
+            profile: profileData
+        }
+    }
+}
